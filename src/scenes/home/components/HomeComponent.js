3162394ddaff12d0d3dcdbcba59666ed6e3c0165
andreepratama27 <andreepratama27@gmail.com>
2018-07-13 22:34:40 +0700

    init home component

--- src/scenes/home/components/HomeComponent.js
+++ src/scenes/home/components/HomeComponent.js
@@ -1,8 +1,39 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 class HomeComponent extends Component {
   render() {
-    return <h1>Andre</h1>;
+    return (
+      <Fragment>
+        <nav
+          className="navbar is-info"
+          role="navigation"
+          aria-label="main navigation"
+        >
+          <div className="navbar-brand">
+            <a href="#" className="navbar-item">
+              Exchange Rate
+            </a>
+
+            <a
+              role="button"
+              className="navbar-burger"
+              aria-label="menu"
+              aria-expanded="false"
+            >
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+            </a>
+          </div>
+        </nav>
+
+        <main className="column content">
+          <div className="card">
+            <div className="card-header">Andre</div>
+          </div>
+        </main>
+      </Fragment>
+    );
   }
 }
 
